cd05d9bb86a3a5c919812b9cf59eb6372ecf3d4b
Sampah Francis <francissampah2@gmail.com>
2022-10-19 23:37:29 +0000

    Add and Delete products functionality

--- pages/products/all-products.js
+++ pages/products/all-products.js
@@ -1,97 +1,12 @@
-import { Table, Input, Modal, Button, Popconfirm, Form, Card } from 'antd';
+import { Table, Input, Modal, Button, Popconfirm, Form, Card, Select, Message } from 'antd';
+import Router from 'next/router';
 import withAuth from '../../hoc/withAuth';
+import apiClient from '../../apis/client';
+import reqwest from 'reqwest';
 
 const FormItem = Form.Item;
 const { TextArea } = Input;
-const EditableContext = React.createContext();
-
-const EditableRow = ({ form, index, ...props }) => (
-  <EditableContext.Provider value={form}>
-    <tr {...props} />
-  </EditableContext.Provider>
-);
-
-const EditableFormRow = Form.create()(EditableRow);
-
-class EditableCell extends React.Component {
-  state = {
-    editing: false,
-  };
-
-  componentDidMount() {
-    if (this.props.editable) {
-      document.addEventListener('click', this.handleClickOutside, true);
-    }
-  }
-
-  componentWillUnmount() {
-    if (this.props.editable) {
-      document.removeEventListener('click', this.handleClickOutside, true);
-    }
-  }
-
-  toggleEdit = () => {
-    const editing = !this.state.editing;
-    this.setState({ editing }, () => {
-      if (editing) {
-        this.input.focus();
-      }
-    });
-  };
-
-  handleClickOutside = (e) => {
-    const { editing } = this.state;
-    if (editing && this.cell !== e.target && !this.cell.contains(e.target)) {
-      this.save();
-    }
-  };
-
-  save = () => {
-    const { record, handleSave } = this.props;
-    this.form.validateFields((error, values) => {
-      if (error) {
-        return;
-      }
-      this.toggleEdit();
-      handleSave({ ...record, ...values });
-    });
-  };
-
-  render() {
-    const { editing } = this.state;
-    const { editable, dataIndex, title, record, index, handleSave, ...restProps } = this.props;
-    return (
-      <td ref={(node) => (this.cell = node)} {...restProps}>
-        {editable ? (
-          <EditableContext.Consumer>
-            {(form) => {
-              this.form = form;
-              return editing ? (
-                <FormItem style={{ margin: 0 }}>
-                  {form.getFieldDecorator(dataIndex, {
-                    rules: [
-                      {
-                        required: true,
-                        message: `${title} is required.`,
-                      },
-                    ],
-                    initialValue: record[dataIndex],
-                  })(<Input ref={(node) => (this.input = node)} onPressEnter={this.save} />)}
-                </FormItem>
-              ) : (
-                <div className='editable-cell-value-wrap' style={{ paddingRight: 24 }} onClick={this.toggleEdit}>
-                  {restProps.children}
-                </div>
-              );
-            }}
-          </EditableContext.Consumer>
-        ) : (
-          restProps.children
-        )}
-      </td>
-    );
-  }
-}
+const Option = Select.Option;
 
 const ProductAddForm = Form.create()(
   class extends React.Component {
@@ -102,7 +17,7 @@ const ProductAddForm = Form.create()(
         <Modal visible={visible} title='Add a new product' okText='Add product' onCancel={onCancel} onOk={onCreate}>
           <Form layout='vertical'>
             <FormItem label='Product name'>
-              {getFieldDecorator('Product name', {
+              {getFieldDecorator('product_name', {
                 rules: [
                   {
                     required: true,
@@ -110,6 +25,23 @@ const ProductAddForm = Form.create()(
                   },
                 ],
               })(<Input />)}
+            </FormItem>
+            <FormItem label='Product category'>
+              {getFieldDecorator('category_id', {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please category for Product!',
+                  },
+                ],
+              })(
+                <Select>
+                  <Option value='1'>category 1</Option>
+                  <Option value='2'>category 2</Option>
+                  <Option value='3'>category 3</Option>
+                  <Option value='4'>category 4</Option>
+                </Select>
+              )}
             </FormItem>
             <FormItem label='Description'>{getFieldDecorator('description')(<TextArea rows={4} />)}</FormItem>
           </Form>
@@ -119,16 +51,20 @@ const ProductAddForm = Form.create()(
   }
 );
 
-class EditableTable extends React.Component {
+class ProductsTable extends React.Component {
   state = {
     visible: false,
+    dataSource: [],
+    loading: false,
+    count: 0,
   };
+
   constructor(props) {
     super(props);
     this.columns = [
       {
         title: 'Product name',
-        dataIndex: 'name',
+        dataIndex: 'product_name',
         width: '30%',
         editable: true,
       },
@@ -139,63 +75,31 @@ class EditableTable extends React.Component {
       {
         title: 'Actions',
         dataIndex: 'actions',
-        width: '20%',
+        width: '15%',
         render: (text, record) =>
           this.state.dataSource.length >= 1 ? (
-            <Popconfirm title='Sure to delete?' onConfirm={() => this.handleDelete(record.key)}>
+            <Popconfirm title='Sure to delete?' onConfirm={() => this.handleDelete(record.product_id)}>
               <a href='javascript:;'>Delete</a>
             </Popconfirm>
           ) : null,
       },
     ];
-
-    this.state = {
-      dataSource: [
-        {
-          key: '0',
-          name: 'Edward King 0',
-          age: '32',
-          address: 'London, Park Lane no. 0',
-        },
-        {
-          key: '1',
-          name: 'Edward King 1',
-          age: '32',
-          address: 'London, Park Lane no. 1',
-        },
-      ],
-      count: 2,
-    };
   }
 
-  handleDelete = (key) => {
+  handleDelete = (id) => {
     const dataSource = [...this.state.dataSource];
-    this.setState({ dataSource: dataSource.filter((item) => item.key !== key) });
-  };
+    this.setState({ dataSource: dataSource.filter((item) => item.product_id !== id) });
 
-  handleAdd = () => {
-    const { count, dataSource } = this.state;
-    const newData = {
-      key: count,
-      name: `Edward King ${count}`,
-      age: 32,
-      address: `London, Park Lane no. ${count}`,
-    };
-    this.setState({
-      dataSource: [...dataSource, newData],
-      count: count + 1,
+    reqwest({
+      url: 'http://localhost:3000/api/v1/products/deleteProduct',
+      method: 'DELETE',
+      type: 'json',
+      data: {
+        product_id: id,
+      },
+    }).then((data) => {
+      Message.success(`Deleted successfully`).then(() => Router.push('/products/all-products'));
     });
-  };
-
-  handleSave = (row) => {
-    const newData = [...this.state.dataSource];
-    const index = newData.findIndex((item) => row.key === item.key);
-    const item = newData[index];
-    newData.splice(index, 1, {
-      ...item,
-      ...row,
-    });
-    this.setState({ dataSource: newData });
   };
 
   showModal = () => {
@@ -208,12 +112,18 @@ class EditableTable extends React.Component {
 
   handleCreate = () => {
     const form = this.formRef.props.form;
-    form.validateFields((err, values) => {
-      if (err) {
-        return;
-      }
-
-      console.log('Received values of form: ', values);
+    form.validateFields(async (err, values) => {
+      if (err) return;
+      reqwest({
+        url: 'http://localhost:3000/api/v1/products/addProduct',
+        method: 'POST',
+        type: 'json',
+        data: {
+          ...values,
+        },
+      }).then((data) => {
+        Message.success(`${data.data.name} added successfully`).then(() => Router.push('/products/all-products'));
+      });
       form.resetFields();
       this.setState({ visible: false });
     });
@@ -222,14 +132,28 @@ class EditableTable extends React.Component {
   saveFormRef = (formRef) => {
     this.formRef = formRef;
   };
+
+  fetch = (params = {}) => {
+    console.log('params:', params);
+    this.setState({ loading: true });
+    reqwest({
+      url: 'http://localhost:3000/api/v1/products/getProducts',
+      method: 'GET',
+      type: 'json',
+    }).then((data) => {
+      this.setState({
+        loading: false,
+        dataSource: data.data,
+        count: data.data.length,
+      });
+    });
+  };
+
+  componentDidMount() {
+    this.fetch();
+  }
   render() {
     const { dataSource } = this.state;
-    const components = {
-      body: {
-        row: EditableFormRow,
-        cell: EditableCell,
-      },
-    };
     const columns = this.columns.map((col) => {
       if (!col.editable) {
         return col;
@@ -246,7 +170,13 @@ class EditableTable extends React.Component {
       };
     });
     return (
-      <Card bodyStyle={{ padding: 10 }} id='products-page'>
+      <Card
+        bodyStyle={{
+          padding: 10,
+          minHeight: '100vh',
+        }}
+        id='products-page'
+      >
         <div>
           <Button type='primary' onClick={this.showModal} style={{ marginBottom: 16 }}>
             New Product
@@ -257,17 +187,11 @@ class EditableTable extends React.Component {
             onCancel={this.handleCancel}
             onCreate={this.handleCreate}
           />
-          <Table
-            components={components}
-            rowClassName={() => 'editable-row'}
-            bordered
-            dataSource={dataSource}
-            columns={columns}
-          />
+          <Table bordered dataSource={dataSource} loading={this.state.loading} columns={columns} />
         </div>
       </Card>
     );
   }
 }
 
-export default withAuth(EditableTable);
+export default withAuth(ProductsTable);
